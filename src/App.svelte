f9cb9820d11f33ad5363f5f33765eb172c51a769
YoYolops <yohanhxh@gmail.com>
2021-05-28 21:17:46 -0300

    adicionando TS support

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,24 @@
-<script>
-	export let name;
+<script lang="ts">
+	import ItemCard from "./ItemCard.svelte";
+
+	$: counter = 0
+
+	function addCounter() {
+		counter += 1
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<ItemCard />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	* {
+		display:flex;
+		width: 100%;
+		justify-content: center;
+		align-items: center;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		font-size: 55px;
 	}
 </style>
---
+++ src/ItemCard.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+<main>
+    <h1>Título</h1>
+    <div>Imagem de Background</div>
+    <div>Bloco descrição</div>
+    <div>bloco atk, def, consumivel, peso</div>
+</main>
+
+<style>
+    main {
+        height: 300px;
+        width: 200px;
+        border: 2px solid black;
+        border-radius: 5px;
+        display:flex;
+        flex-direction: column;
+    }
+
+    h1 {
+        font-size: 1rem;
+        padding: 2px;
+    }
+
+</style>
